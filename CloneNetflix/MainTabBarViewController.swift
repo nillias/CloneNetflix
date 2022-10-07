9614d533f1ac2659c2d5a19e80a3d849fa703977
Nillia Sousa <nilliasusa@gmail.com>
2022-10-07 16:21:29 -0300

    Primeiro commit

---
+++ CloneNetflix/MainTabBarViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CloneNetflix
+//
+//  Created by Nillia Sousa on 06/10/22.
+//
+
+import UIKit
+
+class mainTabbarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemYellow
+    }
+
+
+}
+
